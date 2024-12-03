a84c6626efa2125f795f52dfe3fab55a2d181fcf
Wesley <wesleyymelo2@hotmail.com>
2024-12-03 20:55:45 -0300

    Configurei o application.properties

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("Diferentao");
+		System.out.println("Trabalho Wesley Melo");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
